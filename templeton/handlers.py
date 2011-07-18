1d0721295e025646a848f4237d506c4199cb617c
Mark Cote <mcote@mozilla.com>
2011-07-18 01:07:24 -0400

    Don't patch unless middleware module specifically included. Added @get_json(). Preferred way to wrap JSON is now the decorator.

--- templeton/handlers.py
+++ templeton/handlers.py
@@ -6,9 +6,18 @@ import posixpath
 import urlparse
 import web
 
-class JsonHandler(object):
+def get_request_parms():
+    """ Parses search string and body as JSON. """
+    try:
+        body = json.loads(web.data())
+    except (TypeError, ValueError):
+        body = None
+    return (urlparse.parse_qs(web.ctx.query[1:], True), body)
 
-    def GET(self):
+
+def get_json(func):
+    """ Translates results of 'func' into a JSON request. """
+    def wrap(*a, **kw):
         if web.ctx.path[-1] != '/':
             # redirect all api calls to slash-terminated pages, for
             # consistency
@@ -16,14 +25,25 @@ class JsonHandler(object):
                                               web.ctx.path) + '/' +
                                web.ctx.query)
         try:
-            body = json.loads(web.data())
-        except (TypeError, ValueError):
-            body = None
-        results = json.dumps(self._GET(urlparse.parse_qs(web.ctx.query[1:],
-                                                         True), body))
+            results = json.dumps(func(*a, **kw))
+        except KeyboardInterrupt:
+            # Allow keyboard interrupts through for debugging.
+            raise
         web.header('Content-Length', len(results))
         web.header('Content-Type', 'application/json; charset=utf-8')
         return results
+    return wrap
+
+
+class JsonHandler(object):
+    """
+    Deprecated; use decorators instead, allowing for mixed response types.
+    """
+
+    @get_json
+    def GET(self):
+        args, body = get_request_parms()
+        return self._GET(args, body)
 
     def _GET(self, params, body):
         raise NotImplementedError
